339a9e4e4f3d719356a68c4b3f72aeb61d3f9e0f
Stephanie Nemeth <traumverloren@gmail.com>
2015-01-11 23:35:22 +0100

    added vote.rb model and migration

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,6 @@
 class Link < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :votes
   validates :title, :url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :links, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :votes
 
 
   # Include default devise modules. Others available are:
