42eb3f2347719fd7990eeee4c760cf2aaf979dfd
Stephanie Nemeth <traumverloren@gmail.com>
2015-01-12 15:05:52 +0100

    limited user to vote 1 per link, added up/down vote ct, glyphicons, added bootstrap panels in pages#index

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,15 @@
 class VotesController < ApplicationController
+  before_filter :authenticate_user!
 
   def create
-    @vote = current_user.votes.new(vote_params)
-    @vote.save
+    @vote = Vote.where(:link_id => params[:vote][:link_id], :user_id => current_user.id).first
+    if @vote
+      @vote.up = params[:vote][:up]
+      @vote.save
+    else
+      @vote = current_user.votes.new(vote_params)
+      @vote.save
+    end
     redirect_to :back
   end
 
